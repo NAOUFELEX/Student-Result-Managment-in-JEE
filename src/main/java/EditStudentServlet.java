10ef496e0e743d641b8a01cb772dfe5b0bacbb5c
naoufel <77999196+NAOUFELEX@users.noreply.github.com>
2023-05-05 14:58:37 +0100

    Update EditStudentServlet.java

--- src/main/java/EditStudentServlet.java
+++ src/main/java/EditStudentServlet.java
@@ -1,1 +1,90 @@
 
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class EditStudentServlet
+ */
+public class EditStudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private static final String DB_URL = "jdbc:mysql://localhost:3308/notes";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditStudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int studentId = Integer.parseInt(request.getParameter("id"));
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String query = "SELECT * FROM students WHERE id = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, studentId);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+                String gender = rs.getString("gender");
+                String username = rs.getString("username");
+
+                out.println("<html><head><title>Edit Student</title></head><body>");
+                out.println("<h1>Edit Student</h1>");
+                out.println("<form action='updateStudent' method='POST'>");
+                out.println("<input type='hidden' name='id' value='" + studentId + "'/>");
+                out.println("First Name: <input type='text' name='firstName' value='" + firstName + "'/><br/><br/>");
+                out.println("Last Name: <input type='text' name='lastName' value='" + lastName + "'/><br/><br/>");
+                out.println("Gender: <input type='text' name='gender' value='" + gender + "'/><br/><br/>");
+                out.println("Username: <input type='text' name='username' value='" + username + "'/><br/><br/>");
+                out.println("<input type='submit' value='Update'/>");
+                out.println("</form>");
+                out.println("</body></html>");
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
